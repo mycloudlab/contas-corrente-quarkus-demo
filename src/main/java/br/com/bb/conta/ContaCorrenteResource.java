1d771db5cdf806a038d5ef4057c91f5024bb5b53
Carlos Alberto (Euprogramador) <euprogramador@gmail.com>
2021-08-26 16:19:33 -0300

    Update ContaCorrenteResource.java

--- src/main/java/br/com/bb/conta/ContaCorrenteResource.java
+++ src/main/java/br/com/bb/conta/ContaCorrenteResource.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -17,6 +18,7 @@ import javax.ws.rs.core.Response;
 public class ContaCorrenteResource {
 
   Set<ContaCorrente> contas = new HashSet<>();
+   private long delay;
 
   public ContaCorrenteResource(){
     contas.add(new ContaCorrente("00045789100", "001", "145784-X", BigDecimal.valueOf(1500.00)));
@@ -28,12 +30,27 @@ public class ContaCorrenteResource {
     System.out.println("Recebendo request");
     return contas;
   }
+  
+  @GET
+  @Path("/delay")
+  @Produces(MediaType.APPLICATION_JSON)
+  public void delay(@QueryParam("delay") long delay) {
+    System.out.println("delay configurado:" + delay);
+    this.delay = delay;
+  }
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("/{cpf}")
   public Response retrive(@PathParam("cpf") String cpf) {
-    System.out.println("Recebendo request");
+     System.out.println("Recebendo request");
+    try {
+      System.out.println("aguardando..." + delay);
+      Thread.currentThread().sleep(delay);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
     List<ContaCorrente> contas = this.contas.stream().filter((p)->cpf.equals(p.getCpf()))  .collect(Collectors
         .toCollection(ArrayList::new));
 
